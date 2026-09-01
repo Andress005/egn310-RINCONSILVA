c81ffaaf1c33e4cf1a35e55438ec667c567d9d0f
Andress005 <153233451+Andress005@users.noreply.github.com>
2026-09-01 12:49:47 -0400

    Add initial collection and sorting script

---
+++ .venv/main.py
@@ -0,0 +1,20 @@
+items = [
+    "Chevrolet",
+    "Mazda",
+    "Toyota",
+    "Kia",
+    "Hyundai",
+    "Porche",
+    "Ferrari",
+    "Honda"
+]
+
+print("original order:")
+print(items)
+
+print("\nAlphabetical order:")
+print(sorted(items))
+
+print("\nReverse order:")
+print(items.sort(reverse=True))
+
