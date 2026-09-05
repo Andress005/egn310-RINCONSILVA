523265a523111f63b8d7d3bb0468c7d57cd3ee5e
Andress005 <153233451+Andress005@users.noreply.github.com>
2026-09-05 13:02:59 -0400

    8 items

--- main.py
+++ main.py
@@ -9,13 +9,3 @@ items = [
     "Honda"
 ]
 
-print("original order:")
-print(items)
-
-print("\nAlphabetical order:")
-print(sorted(items))
-
-print("\nReverse order:")
-items.sort(reverse=True)
-print(items)
-
