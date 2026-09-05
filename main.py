d0a6db55d41734a7becb00034e3e5e41d7b22557
Andress005 <153233451+Andress005@users.noreply.github.com>
2026-09-05 13:03:31 -0400

    8 items

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ items = [
     "Ferrari",
     "Honda"
 ]
+print("original order:")
+print(items)
 
+print("\nAlphabetical order:")
+print(sorted(items))
+
+print("\nReverse order:")
+items.sort(reverse=True)
+print(items)
